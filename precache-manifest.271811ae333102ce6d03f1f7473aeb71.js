5c839e833d2c7e63f86316c782cadfc1bbb310f7
Soomin Kimu <soominkimu@gmail.com>
2020-01-09 16:13:35 +0900

    Babylon 3D Baby

---
+++ precache-manifest.271811ae333102ce6d03f1f7473aeb71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d942763fc983da7c6be447a5a8ccf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1388d5107c09f8847b",
+    "url": "/static/css/main.e93c75b1.chunk.css"
+  },
+  {
+    "revision": "00c43c71716f630a0c0c",
+    "url": "/static/js/2.b3acf9e6.chunk.js"
+  },
+  {
+    "revision": "2aa45c9749f8d8b13941ffe847cda6bc",
+    "url": "/static/js/2.b3acf9e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d1388d5107c09f8847b",
+    "url": "/static/js/main.3acfed4a.chunk.js"
+  },
+  {
+    "revision": "0ed8bc9b6c1a3ceb9dc4",
+    "url": "/static/js/runtime-main.a13ab780.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8de96f84c3df9c0ec5e58bf90bc49359.js"
+  "/precache-manifest.271811ae333102ce6d03f1f7473aeb71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
